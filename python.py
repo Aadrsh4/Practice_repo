8c9a9c2219d1408e1ddf8e14befc4e19557fd6bd
Aadrsh4 <aadrsh44400@gmai.com>
2024-08-11 07:38:19 +0530

    added

--- python.py
+++ python.py
@@ -4,4 +4,7 @@ gufv
 bk
 i am pushing this is QA let'see what happendhvhv
 
-hlo added this one 11082024
+hlo added this one 11082024   
+
+i have created this branch and i will menrge this to main branch.
+
